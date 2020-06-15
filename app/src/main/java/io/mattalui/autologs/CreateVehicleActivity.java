ee553b8c43bef5ad97a70445da50be9252391a37
Matthew Hummer <biggreyson@gmail.com>
2020-06-14 22:12:51 -0600

    feat(#4): add ability to create a vehicle

---
+++ app/src/main/java/io/mattalui/autologs/CreateVehicleActivity.java
@@ -0,0 +1,44 @@
+package io.mattalui.autologs;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+
+import io.mattalui.autologs.models.Vehicle;
+import io.mattalui.autologs.services.AutologsServices;
+
+public class CreateVehicleActivity extends VehicleActivity {
+    @Override
+    protected void fetchVehicle() {
+        vehicle = new Vehicle();
+    }
+
+    public void save(View v){
+        vehicle.make = makeInput.getText().toString();
+        vehicle.model = modelInput.getText().toString();
+        vehicle.year = yearInput.getText().toString();
+        vehicle.nickname = nicknameInput.getText().toString();
+        vehicle.description = descriptionInput.getText().toString();
+
+        final Vehicle newVehicle = vehicle;
+        final CreateVehicleActivity that = this;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final Vehicle createdVehicle = new AutologsServices(usertoken).createVehicle(newVehicle);
+
+                that.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast myToast = Toast.makeText(that, "Successfully created " +  createdVehicle.toString(), Toast.LENGTH_LONG);
+                        myToast.show();
+
+                        Intent intent = new Intent(that, ViewVehicles.class);
+                        startActivity(intent);
+                    }
+                });
+            }
+        }).start();
+    }
+}
