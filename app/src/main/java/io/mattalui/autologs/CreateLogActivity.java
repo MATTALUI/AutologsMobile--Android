76fd3482e3349c009cef5b6bf8347ae047833c02
ehalterman <hal17034@byui.edu>
2020-07-07 11:22:19 -0600

    trying again

--- app/src/main/java/io/mattalui/autologs/CreateLogActivity.java
+++ app/src/main/java/io/mattalui/autologs/CreateLogActivity.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Toast;
 
-import io.mattalui.autologs.models.AutoLog;
+import io.mattalui.autologs.models.State;
+import io.mattalui.autologs.models.Vehicle;
 import io.mattalui.autologs.services.AutologsServices;
-
+import io.mattalui.autologs.models.AutoLog;
 /**
  *Creates the log from the data collected in LogActivity
  */
@@ -16,20 +17,33 @@ public class CreateLogActivity extends LogActivity {
         log = new AutoLog();
     }
 
-    public void save(View v){
-        log.fillupAmount = fillupAmountInput.getText().toString();
-        log.fillupCost = fillupCostInput.getText().toString();
-        log.miles = milesInput.getText().toString();
+    public void saveLog(View v){
+        System.out.println("Saving");
+        String vehicleName = vehicleSpinner.getSelectedItem().toString();
+        System.out.println(vehicleName);
+        for (Vehicle vehicle : State.getState().getVehicles()){
+            if(vehicle.toString().equals(vehicleName)){
+                vehicle.display();
+                log.vehicle = vehicle.id;
+                break;
+            }
+        }
+        //log.vehicle = Integer.parseInt(vehicleSpinner.getSelectedItem().toString());
+        log.fillupAmount = Float.parseFloat(fillupAmountInput.getText().toString());
+        log.fillupCost = Float.parseFloat(fillupCostInput.getText().toString());
+        log.miles = Float.parseFloat(milesInput.getText().toString());
         log.note = noteInput.getText().toString();
         log.location = locationInput.getText().toString();
+        System.out.println("Create Log Activity");
+        log.display();
 
-        final Log newAutoLog = log;
+        final AutoLog newAutoLog = log;
         final CreateLogActivity that = this;
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                final Log createdLog = new AutologsServices(usertoken).createLog(newAutoLog);
+                final AutoLog createdLog = new AutologsServices(usertoken).createLog(newAutoLog);
 
                 that.runOnUiThread(new Runnable() {
                     @Override
@@ -44,4 +58,5 @@ public class CreateLogActivity extends LogActivity {
             }
         }).start();
     }
+
 }
